cb74285e2b84a5235846775e3d8e7fdfed2b56cd
karinareyes25 <karinalrg16@gmail.com>
2025-02-10 22:43:13 +0100

    necesary test added. Test run correctly

--- src/main/java/dev/karina/imc_java/CalculatorIMC.java
+++ src/main/java/dev/karina/imc_java/CalculatorIMC.java
@@ -14,6 +14,31 @@ public class CalculatorIMC {
         return weight / Math.pow(height, 2);
     }
 
-    
+    public String clasificateIMC() {
+        double imc = calculatedIMC();
 
+        if (imc < 16) {
+            return "Severe thinness";
+        } else if (imc >= 16 && imc < 17) {
+            return "Moderate thinness";
+        } else if (imc >= 17 && imc < 18.5) {
+            return "Mild thinness";
+        } else if (imc >= 18.5 && imc < 25) {
+            return "Normal weight";
+        } else if (imc >= 25 && imc < 30) {
+            return "Overweight";
+        } else if (imc >= 30 && imc < 35) {
+            return "Moderate obesity";
+        } else if (imc >= 35 && imc < 40) {
+            return "Mild obesity";
+        } else {
+            return "Morby obesity";
+        }
+    }
+
+    public static void main(String[] args) {
+        CalculatorIMC imcCImc = new CalculatorIMC(70, 1.75);
+        System.out.println("IMC: " + imcCImc.calculatedIMC());
+        System.out.println("Clasificación: " + imcCImc.clasificateIMC());
+    }
 }
--- src/test/java/dev/karina/imc_java/CalculatorIMCTest.java
+++ src/test/java/dev/karina/imc_java/CalculatorIMCTest.java
@@ -25,7 +25,62 @@ public class CalculatorIMCTest {
     @Test
     @DisplayName("Clasifica correctamente: delgadez severa")
     void testClasificateSevereThinness() {
-        CalculatorIMC imcCalc = new CalculatorIMC(48, 1.75);
+        CalculatorIMC imcCalc = new CalculatorIMC(45, 1.75);
         assertEquals("Severe thinness", imcCalc.clasificateIMC());
     }
+
+
+    @Test
+    @DisplayName("Clasifica correctamente: delgadez moderada")
+    void testClasificateModerateThinness() {
+        CalculatorIMC imcCalc = new CalculatorIMC(50, 1.75);
+        assertEquals("Moderate thinness", imcCalc.clasificateIMC());
+    }
+
+    @Test
+    @DisplayName("Clasifica correctamente: delgadez leve")
+    void testClasificateMildThinness() {
+        CalculatorIMC imcCalc = new CalculatorIMC(55, 1.75);
+        assertEquals("Mild thinness", imcCalc.clasificateIMC());
+    }
+
+    @Test
+    @DisplayName("Clasifica correctamente: peso normal")
+    void testClasificateNormalWeight() {
+        CalculatorIMC imcCalc = new CalculatorIMC(70, 1.75);
+        assertEquals("Normal weight", imcCalc.clasificateIMC());
+    }
+
+    @Test
+    @DisplayName("Clasifica correctamente: sobrepeso")
+    void testClasificateOverweight() {
+        CalculatorIMC imcCalc = new CalculatorIMC(85, 1.75);
+        assertEquals("Overweight", imcCalc.clasificateIMC());
+    }
+
+    @Test
+    @DisplayName("Clasifica correctamente: obesidad moderada")
+    void testClasificateModerateObesity() {
+        CalculatorIMC imcCalc = new CalculatorIMC(95, 1.75);
+        assertEquals("Moderate obesity", imcCalc.clasificateIMC());
+    }
+
+    @Test
+    @DisplayName("Clasifica correctamente: obesidad leve")
+    void testClasificateMildObesity() {
+        CalculatorIMC imcCalc = new CalculatorIMC(110, 1.75);
+        assertEquals("Mild obesity", imcCalc.clasificateIMC());
+    }
+
+    @Test
+    @DisplayName("Clasifica correctamente: obesidad mórbida")
+    void testClasificateMorbyObesity() {
+        CalculatorIMC imcCalc = new CalculatorIMC(130, 1.75);
+        assertEquals("Morby obesity", imcCalc.clasificateIMC());
+    }
+
+
+
+
+
 }
