0a48fecf9c43146b88fa02beb375bbb8ceee468c
karinareyes25 <karinalrg16@gmail.com>
2025-02-10 19:17:36 +0100

    testCalculatorIMC run correctly

--- src/main/java/dev/karina/imc_java/CalculatorIMC.java
+++ src/main/java/dev/karina/imc_java/CalculatorIMC.java
@@ -1,5 +1,18 @@
 package dev.karina.imc_java;
 
 public class CalculatorIMC {
+
+    private double weight;
+    private double height;
+
+    public CalculatorIMC(double weight, double height){
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public double calculatedIMC(){
+        return weight / Math.pow(height, 2);
+    }
     
+
 }
