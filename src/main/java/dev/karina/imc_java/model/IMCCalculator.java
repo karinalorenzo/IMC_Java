4783031f437f5f16618b8ef0934368424e667dd6
karinareyes25 <karinalrg16@gmail.com>
2025-02-10 17:29:54 +0100

    delete all files and init the exercice again

--- src/main/java/dev/karina/imc_java/model/IMCCalculator.java
+++
@@ -1,7 +0,0 @@
-package dev.karina.imc_java.model;
-
-
-    public interface IMCCalculator {
-        double calculateIMC(double weight, double height);
-    }
-
--- src/main/java/dev/karina/imc_java/model/IMCCategorizer.java
+++
@@ -1,5 +0,0 @@
-package dev.karina.imc_java.model;
-
-public interface IMCCategorizer {
-    double calculateIMC(double weight, double height);
-}
